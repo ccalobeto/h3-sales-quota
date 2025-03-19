bf97fffe12a471704ec7bcd5c1e74054292b2391
ccalobeto <calobeto.leon@gmail.com>
2025-03-19 12:31:53 -0500

    add skeleton

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import '$lib/styles/app.css';
+	// import Header from '$lib/components/Header.svelte';
+
+	export let children;
+</script>
+
+<!-- <Header /> -->
+<main id="content">
+	{@render children?.()}
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,22 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { setContext } from 'svelte';
+	import Meta from '$lib/components/Meta.svelte';
+	import Index from '$lib/components/Index.svelte';
+	import copy from '$lib/data/copy.json';
+
+	export let data;
+
+	const preloadFont = [
+		'https://pudding.cool/assets/fonts/tiempos/TiemposTextWeb-Regular.woff2',
+		'https://pudding.cool/assets/fonts/tiempos/TiemposTextWeb-Bold.woff2',
+		'https://pudding.cool/assets/fonts/atlas/AtlasGrotesk-Regular-Web.woff2',
+		'https://pudding.cool/assets/fonts/atlas/AtlasGrotesk-Bold-Web.woff2'
+	];
+
+	const { title, description, url, keywords } = copy;
+	setContext('copy', copy);
+	setContext('data', data);
+</script>
+
+<Meta {title} {description} {url} {preloadFont} {keywords} />
+<Index />
