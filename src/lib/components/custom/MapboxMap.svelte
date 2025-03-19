57f904bcf511d2d21e73b29e73d06b194657d036
ccalobeto <calobeto.leon@gmail.com>
2025-03-19 12:32:27 -0500

    add mapbox component

---
+++ src/lib/components/custom/MapboxMap.svelte
@@ -0,0 +1,12 @@
+<script>
+	export let steps;
+	export let index;
+	export let title;
+</script>
+
+<h1>This is mapbox map!!</h1>
+<h2>{title}</h2>
+
+{#each steps as step}
+	<p>{step}: {index}</p>
+{/each}
