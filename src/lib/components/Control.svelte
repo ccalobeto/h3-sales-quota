aad8d9c3f09671f4e52c20354b3a7e277d825ebe
ccalobeto <calobeto.leon@gmail.com>
2025-03-19 12:32:17 -0500

    add main components

---
+++ src/lib/components/Control.svelte
@@ -0,0 +1,26 @@
+<script>
+	import MapboxMap from './custom/MapboxMap.svelte';
+
+	export let copy;
+</script>
+
+<div class="charts-container">
+	<!-- {console.log("Charts: step => ", step)} -->
+	{#each copy.body as item, index}
+		<div class="type-{item['type']}">
+			{#if item.type === 'text'}
+				<p class="text-blocks">{@html item.value}</p>
+			{/if}
+			{#if item.type === 'mapbox-chart-1'}
+				{@const steps = item.value.steps}
+				{@const title = item.title}
+				<MapboxMap {steps} {index} {title} />
+			{/if}
+			{#if item.type === 'map-box-chart-2'}
+				{@const steps = item.value.steps}
+				{@const title = item.title}
+				<MapboxMap {steps} {index} {title} />
+			{/if}
+		</div>
+	{/each}
+</div>
---
+++ src/lib/components/Index.svelte
@@ -0,0 +1,23 @@
+<script>
+	import { getContext } from 'svelte';
+	import Control from './Control.svelte';
+
+	const copy = getContext('copy');
+</script>
+
+<main>
+	<section>
+		<h1>{copy.title}</h1>
+		<p>{@html copy.description}</p>
+		<img src="missing-child.jpg" alt="" />
+		<p class="byline">{@html copy.byline}</p>
+	</section>
+
+	<article>
+		<Control {copy} />
+	</article>
+
+	<div class="footer">
+		<p class="made-with">{@html copy.footer}</p>
+	</div>
+</main>
