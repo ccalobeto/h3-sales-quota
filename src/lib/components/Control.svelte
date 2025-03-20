3a76f14415cc1c139c1107ab3f7a95eb67b7fe37
ccalobeto <calobeto.leon@gmail.com>
2025-03-20 18:10:23 -0500

    update control component

--- src/lib/components/Control.svelte
+++ src/lib/components/Control.svelte
@@ -1,7 +1,27 @@
 <script>
-	import MapboxMap from './custom/MapboxMap.svelte';
+	import { onMount } from 'svelte';
+	import { bbox } from 'topojson-client';
+	import { geoTypes } from '$lib/config';
+
+	import HexagonMap from './custom/HexagonMap.svelte';
+
+	import topo from '$lib/data/peru-100k.json';
+	import { hexagons, geoData } from '$lib/data/cookedData';
 
 	export let copy;
+
+	let bounds;
+	const geoPoints = geoData();
+	const h3Hexagons = hexagons();
+
+	// console.log('Control: hexagons => ', h3Hexagons);
+
+	async function init() {
+		bounds = bbox(topo);
+	}
+
+	onMount(init);
+	$: console.log('Control: bounds => ', bounds);
 </script>
 
 <div class="charts-container">
@@ -11,15 +31,15 @@
 			{#if item.type === 'text'}
 				<p class="text-blocks">{@html item.value}</p>
 			{/if}
-			{#if item.type === 'mapbox-chart-1'}
+			<!-- {#if item.type === 'mapbox-chart-1'}
 				{@const steps = item.value.steps}
 				{@const title = item.title}
 				<MapboxMap {steps} {index} {title} />
-			{/if}
+			{/if} -->
 			{#if item.type === 'map-box-chart-2'}
 				{@const steps = item.value.steps}
 				{@const title = item.title}
-				<MapboxMap {steps} {index} {title} />
+				<HexagonMap data={h3Hexagons} {bounds} />
 			{/if}
 		</div>
 	{/each}
