a7991c5a9d02868437b03316c89ef35a055d84cb
ccalobeto <calobeto.leon@gmail.com>
2025-03-20 18:06:21 -0500

    add config file

---
+++ src/lib/config.js
@@ -0,0 +1,8 @@
+export const geoTypes = [
+	{key: "level1", label: "País"},
+	{key: "level2", label: "Departamento"},
+	{key: "level3", label: "Provincia"},
+	{key: "level4", label: "Distrito"},
+];
+
+export const mapStyle = "https://basemaps.cartocdn.com/gl/voyager-gl-style/style.json";
